ff67a850f80614e241c0847f3d671c9e6b6131ba
Tungsten-Wolframite <tungsten2@tungsten2.(none)>
2015-07-31 21:48:47 +0530

    31/7/015

---
+++ WebContent/php/config.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    // Mail Address and Name
+    'sender_email'   => 'lekha.menon@niteshgroup.com',
+    'sender_name'    => 'Wellness-Sutra',
+
+    // Mail Config
+    'mail_type'     => 'mail', // smtp or mail - mail is the php mail function
+    'smtp_server'   => 'smtp.server.com',
+    'smtp_port'     => '25',
+    'smtp_user'     => 'username',
+    'smtp_password' => 'xxxxxxxxxxx',
+
+    // Mail Subjects
+    'contact_form_subject' => 'New message form Welless-Sutra',
+    'newsletter_form_subject' => 'New message form Welless-Sutra',
+    'appointment_form_subject' => 'New appointment request form Welless-Sutra',
+    'appointment_autoresponder_subject' => 'Welless-Sutra:Thanks for your appointment request',
+
+    // Mailchimp
+    'mailchimp_support' => false, // true is activated
+    'mailchimp_api_key' => 'xxxxxx-your-api-key-xxxxxx',
+    'mailchimp_list_id' => 'xxx-list-id-xxx',
+);
